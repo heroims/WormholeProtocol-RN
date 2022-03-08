966c189fc2e6c70864a7667049f14e60cad8990b
heroims <heroims@163.com>
2022-03-08 16:19:55 +0800

    init

---
+++ index.js
@@ -0,0 +1,3 @@
+import BLETransferManager from "./src/BLETransferManager";
+import BLEStore from "./src/BLEStore";
+
---
+++ src/BLETransferManager.js
@@ -0,0 +1,217 @@
+export {BLECentral as BleManager};
+import BLECentral from "react-native-ble-manager/BleManager";
+import BLEPeripheral from 'react-native-ble-peripheral';
+import {
+    NativeEventEmitter,
+    NativeModules,
+  } from 'react-native';
+  
+import { Buffer } from 'buffer';
+
+const EndFlag=0xfe;
+const PartFlag=0xfd;
+// const StartFlag=0xfc;
+const MaxSendSize=20;
+const PushSplitSize=18;
+
+class BLETransferManager {
+    constructor() {
+        this.BLEPeripheralManagerEmitter = new NativeEventEmitter(BLEPeripheral);
+        this.BLECentralManagerEmitter = new NativeEventEmitter(NativeModules.BleManager);
+        this.tmpWaitArr = {};
+    }
+
+    GetBLEPeripheralEmitter(){
+        return this.BLEPeripheralManagerEmitter;
+    }
+
+    GetBLECentralEmitter(){
+        return this.BLECentralManagerEmitter;
+    }
+
+    SetDeviceCharacteristicUUID(uuid){
+        this.deviceCharacteristicUUID = uuid;
+    }
+
+    SetPeripheralName(localName){
+        BLEPeripheral.setName(localName);
+    }
+
+    AddPeripheralServer(serviceUUID,primary=true){
+        BLEPeripheral.addService(serviceUUID, primary);
+    }
+
+    AddPeripheralCharacteristicToService(bleServiceUUID,deviceCharacteristicUUID,permissions,properties){
+        BLEPeripheral.addCharacteristicToService(bleServiceUUID,deviceCharacteristicUUID,permissions,properties)
+    }
+
+    StartTransfer(){
+        return new Promise((fulfill, reject)=>{
+            this.StartPeripheral()
+            .then(res1=>{
+                this.StartCentral()
+                .then(res2=>{
+                    fulfill({'res1':res1,'res2':res2});
+                })
+                .catch(err2=>{
+                    reject(err2);
+                })
+            })
+            .catch(err1=>{
+                reject(err1);
+            });
+        });
+    }
+
+    StartPeripheral(){
+       return BLEPeripheral.start()
+    }
+
+    StartCentral(){
+        return BLECentral.start()
+    }
+
+    Scan(serviceUUIDs,seconds,allowDuplicates, scanningOptions = {}){
+        return BLECentral.scan(serviceUUIDs,seconds,allowDuplicates,scanningOptions);
+    }
+
+    StopScan(){
+        return BLECentral.stopScan();
+    }
+
+    ConnectPeripheral(peripheralID){
+        return BLECentral.connect(peripheralID);
+    }
+
+    DisconnectPeripheral(peripheralID){
+        return BLECentral.disconnect(peripheralID);
+    }
+
+    RetrievePeripheralServices(peripheralID,serviceUUIDs){
+        return BLECentral.retrieveServices(peripheralID,serviceUUIDs);
+    }
+
+    StopNotification(peripheralID,serviceUUID,characteristicUUID){
+        return BLECentral.stopNotification(peripheralID,serviceUUID,characteristicUUID);
+    }
+
+    StartNotification(peripheralID,serviceUUID,characteristicUUIDs){
+        return new Promise((fulfill, reject)=>{
+            this.RetrievePeripheralServices(peripheralID,[serviceUUID])
+            .then((peripheralInfo)=>{
+                var tmpArr = peripheralInfo.characteristics;
+
+                var tmpCharIDs = characteristicUUIDs;
+                while(tmpCharIDs.length>0){
+                    var characteristicUUID = tmpCharIDs.pop();
+                    for (var element of tmpArr) {
+
+                        if(
+                            (element.service.toUpperCase()==serviceUUID.toUpperCase())&&
+                            (characteristicUUID.toUpperCase()==element.characteristic.toUpperCase())
+                        ){
+                            fulfill(element);
+                            console.log('startNotification');
+                            BLECentral.startNotification(peripheralID,serviceUUID,characteristicUUID).then(res=>{
+                                console.log('startNotification sccuess');
+                            })
+                            break;
+                        }
+                    }
+                }
+              
+            })
+            .catch(err=>{
+                reject(err);
+            });
+        });
+    }
+
+    GenerateUUID() {
+        var d = new Date().getTime();
+        var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+          var r = (d + Math.random()*16)%16 | 0;
+          d = Math.floor(d/16);
+          return (c=='x' ? r : (r&0x3|0x8)).toString(16);
+        });
+        return uuid;
+    }
+
+    SendToPeripheral(peripheralID,serviceUUID,characteristicUUID,buffer){
+        console.log('buffer:'+buffer);
+        return BLECentral.write(peripheralID,serviceUUID,characteristicUUID,buffer)
+    }
+
+    SendToCentral(centralIDs,serviceUUID,characteristicUUID,buffer){
+        console.log('buffer:'+buffer);
+
+        BLEPeripheral.sendNotificationToDevices(serviceUUID,characteristicUUID,buffer,centralIDs)
+    }
+
+    TransferSend(buffer, cb){
+        var dataArr=[];
+        var dataBuffer=buffer;
+        if(dataBuffer.length<MaxSendSize){
+          for (const iterator of dataBuffer) {
+            dataArr.push(iterator);
+          }
+          dataArr.push(EndFlag)
+          cb(dataArr)
+        }
+        else
+        {
+          for (let index = 0; index < dataBuffer.length; index++) {
+            const element = dataBuffer[index];
+            
+            if(index==dataBuffer.length-1){
+              dataArr.push(element);
+              dataArr.push(EndFlag);
+              cb(dataArr)
+              dataArr=[];
+            }
+            else if(index%PushSplitSize==0){
+              dataArr.push(PartFlag);
+              cb(dataArr)
+              dataArr=[element];
+            }
+            else{
+              dataArr.push(element);
+            }
+          }
+        }
+    }
+
+    TransferReceive(err,characteristic,cb,tag=''){
+        if (err){
+            cb(err,null)
+        }
+        else{
+            var tmpDataArr=characteristic.value.concat();
+            var tmpDataBuffer=Buffer.from(characteristic.value);
+
+            if(this.tmpWaitArr[characteristic.uuid+tag]==undefined){
+               this.tmpWaitArr[characteristic.uuid+tag]=[];
+            }
+
+            if(tmpDataBuffer[tmpDataBuffer.length-1]==EndFlag){
+              tmpDataArr.pop()
+
+              this.tmpWaitArr[characteristic.uuid+tag]=this.tmpWaitArr[characteristic.uuid+tag].concat(tmpDataArr);
+
+              cb(err,Buffer.from(this.tmpWaitArr[characteristic.uuid+tag]))
+
+              delete this.tmpWaitArr[characteristic.uuid+tag];
+            }
+            else if(tmpDataBuffer[tmpDataBuffer.length-1]==PartFlag){
+              tmpDataArr.pop()
+
+              this.tmpWaitArr[characteristic.uuid+tag]=this.tmpWaitArr[characteristic.uuid+tag].concat(tmpDataArr);
+            }
+            else{
+               cb(err,Buffer.from(tmpDataArr))
+            }
+        }
+    }
+}
+
+export default BLETransferManager = new BLETransferManager();
